abb949905242f08e98222cc8d8fcb4dd303ab808
Mello Birkan <mgbirkan@icloud.com>
2023-11-04 00:18:10 -0300

    Initial Commit

---
+++ iOS17&xcode15-studying/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS17&xcode15-studying
+//
+//  Created by Marcello Gonzatto Birkan on 04/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS17&xcode15-studying/iOS17_xcode15_studyingApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS17_xcode15_studyingApp.swift
+//  iOS17&xcode15-studying
+//
+//  Created by Marcello Gonzatto Birkan on 04/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS17_xcode15_studyingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
