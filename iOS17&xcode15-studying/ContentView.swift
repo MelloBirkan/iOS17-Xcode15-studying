51957ac26840b0cfd1418e28a3155141d5fcf4b9
Mello Birkan <mgbirkan@icloud.com>
2023-11-04 00:22:08 -0300

    `Card` implementado e alguns assets adicionados.

--- iOS17&xcode15-studying/ContentView.swift
+++ iOS17&xcode15-studying/ContentView.swift
@@ -8,17 +8,97 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  var body: some View {
+    ZStack {
+      Image("Image 1")
+        .resizable()
+        .aspectRatio(contentMode: .fill)
+        .frame(height: 300)
+        .clipShape(.rect(cornerRadius: 20))
+      
+      VStack(alignment: .center) {
+        Text("modern architecture, an isometric tiny house, cute illustration, minimalist, vector art, night view")
+          .font(.subheadline)
+        HStack(spacing: 8.0) {
+          VStack(alignment: .leading) {
+            Text("Size")
+              .foregroundColor(.secondary)
+            Text("1024x1024")
+          }
+          .font(.subheadline)
+          .fontWeight(.semibold)
+          
+          Divider()
+          
+          VStack(alignment: .leading) {
+            Text("Type")
+              .foregroundColor(.secondary)
+            Text("Upscale")
+          }
+          .font(.subheadline)
+          .fontWeight(.semibold)
+          
+          Divider()
+          
+          VStack(alignment: .leading) {
+            Text("Date")
+              .foregroundColor(.secondary)
+            Text("Today 5:19")
+          }
+          .font(.subheadline)
+          .fontWeight(.semibold)
         }
-        .padding()
+        .frame(height: 44.0)
+        
+        HStack {
+          HStack {
+            Image(systemName: "ellipsis")
+            Divider()
+            Image(systemName: "sparkle.magnifyingglass")
+            Divider()
+            Image(systemName: "face.smiling")
+          }
+          .padding()
+          .frame(height: 44)
+          .overlay(
+              UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 0, bottomLeading: 20, bottomTrailing: 0, topTrailing: 20))
+                .strokeBorder(linearGradient)
+            )
+          .offset(x: -20, y: 20)
+          
+          Spacer()
+          
+          Image(systemName: "square.and.arrow.down")
+            .padding()
+            .frame(height: 44)
+            .overlay(
+              UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 20, bottomLeading: 0, bottomTrailing: 20, topTrailing: 0))
+                .strokeBorder(linearGradient)
+                  .strokeBorder(linearGradient)
+              )
+            .offset(x:20, y: 20)
+        }
+      }
+      .padding(20.0)
+      .background(.ultraThinMaterial)
+      .overlay(
+          RoundedRectangle(cornerRadius: 20)
+            .strokeBorder(linearGradient)
+        )
+      .cornerRadius(20.0)
+      .padding(20)
+      .offset(y: 80)
     }
+    .frame(maxWidth: 400)
+    .padding(20)
+    .dynamicTypeSize(.xSmall ... .xLarge)
+  }
+  
+  var linearGradient: LinearGradient {
+    LinearGradient(colors: [.clear, .primary.opacity(0.3), .clear], startPoint: .topLeading, endPoint: .bottomTrailing)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
